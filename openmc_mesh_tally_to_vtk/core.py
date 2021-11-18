043c735ee64ff151d04500c5a625566de14ff4d4
Jonathan Shimwell <mail@jshimwell.com>
2021-11-18 18:45:48 +0000

    added unit tests

--- openmc_mesh_tally_to_vtk/core.py
+++ openmc_mesh_tally_to_vtk/core.py
@@ -1,34 +1,20 @@
 import numpy as np
-import vtk
-import math
-import openmc
 import openmc_tally_unit_converter as otuc
 
+from .utils import (
+    _get_mesh_from_tally,
+    _replace_nans_with_zeros,
+    _write_vtk,
+    _find_coords_of_mesh,
+)
 
-# def write_effective_dose_tally_to_vtk(
-# todo add specific converters for dose and dpa
+# def write_effective_dose_mesh_tally_to_vtk(
+# todo add specific converters for dose
 # ):
 
-
-def get_mesh_from_tally(tally):
-
-    if tally.contains_filter(openmc.MeshFilter):
-        mesh_filter = tally.find_filter(filter_type=openmc.MeshFilter)
-    else:
-        msg = "Tally does not contain a MeshFilter"
-        raise ValueError(msg)
-
-    mesh = mesh_filter.mesh
-
-    return mesh
-
-
-def replace_nans_with_zeros(list_of_numbers):
-
-    for counter, i in enumerate(list_of_numbers):
-        if math.isnan(i):
-            list_of_numbers[counter] = 0.0
-    return list_of_numbers
+# def write_dpa_mesh_tally_to_vtk(
+# todo add specific converters for dpa
+# ):
 
 
 def write_mesh_tally_to_vtk(
@@ -49,65 +35,19 @@ def write_mesh_tally_to_vtk(
     tally_data = tally_data.tolist()
     error_data = error_data.tolist()
 
-    mesh = get_mesh_from_tally(tally)
-    tally_data = replace_nans_with_zeros(tally_data)
-    error_data = replace_nans_with_zeros(error_data)
+    mesh = _get_mesh_from_tally(tally)
+    tally_data = _replace_nans_with_zeros(tally_data)
+    print(tally_data)
+    error_data = _replace_nans_with_zeros(error_data)
+    xs, ys, zs = _find_coords_of_mesh(mesh)
 
-    write_vtk(mesh, tally_data, error_data, filename, tally.name)
+    output_filename = _write_vtk(
+        xs=xs,
+        ys=ys,
+        zs=zs,
+        tally_data=tally_data,
+        error_data=error_data,
+        filename=filename,
+        label=tally.name)
 
-    return filename
-
-
-def write_vtk(
-    mesh,
-    tally_data,
-    error_data,
-    filename: str,
-    label: str,
-):
-
-    xs = np.linspace(mesh.lower_left[0], mesh.upper_right[0], mesh.dimension[0] + 1)
-    ys = np.linspace(mesh.lower_left[1], mesh.upper_right[1], mesh.dimension[1] + 1)
-    zs = np.linspace(mesh.lower_left[2], mesh.upper_right[2], mesh.dimension[2] + 1)
-
-    vtk_box = vtk.vtkRectilinearGrid()
-
-    vtk_box.SetDimensions(len(xs), len(ys), len(zs))
-
-    vtk_x_array = vtk.vtkDoubleArray()
-    vtk_x_array.SetName("x-coords")
-    vtk_x_array.SetArray(xs, len(xs), True)
-    vtk_box.SetXCoordinates(vtk_x_array)
-
-    vtk_y_array = vtk.vtkDoubleArray()
-    vtk_y_array.SetName("y-coords")
-    vtk_y_array.SetArray(ys, len(ys), True)
-    vtk_box.SetYCoordinates(vtk_y_array)
-
-    vtk_z_array = vtk.vtkDoubleArray()
-    vtk_z_array.SetName("z-coords")
-    vtk_z_array.SetArray(zs, len(zs), True)
-    vtk_box.SetZCoordinates(vtk_z_array)
-
-    tally = np.array(tally_data)
-    tally_data = vtk.vtkDoubleArray()
-    tally_data.SetName(label)  # TODO allow user specified labels
-    tally_data.SetArray(tally, tally.size, True)
-
-    error = np.array(error_data)
-    error_data = vtk.vtkDoubleArray()
-    error_data.SetName("error_tag")
-    error_data.SetArray(error, error.size, True)
-
-    vtk_box.GetCellData().AddArray(tally_data)
-    vtk_box.GetCellData().AddArray(error_data)
-
-    writer = vtk.vtkRectilinearGridWriter()
-
-    writer.SetFileName(filename)
-
-    writer.SetInputData(vtk_box)
-
-    print("Writing %s" % filename)
-
-    writer.Write()
+    return output_filename
